ccefc6da36936f4a66a0d31d34542ae4130e8bee
yashsuthar7984 <yashsuthar7984@gmail.com>
2025-10-07 12:03:58 +0530

    assignment1

---
+++ Assignments/Assignemnt_1_Task_2.py
@@ -0,0 +1,4 @@
+name1 = input("Enter your name: ")
+name2 = input("Enter your name: ")
+
+print("Hello " + name1 + " " + name2 + "! Welcome to the Python program")
---
+++ Assignments/Assignment_1_Task_1.py
@@ -0,0 +1,10 @@
+
+A = input("enter first number: ")
+B = input("enter second number: ")
+
+A = float(A)
+B = float(B)
+print("addition :", A + B)
+print("subtraction :", A - B)
+print("multiplication :", A * B)
+print("division :", A / B)
